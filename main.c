32b74e29f8bdaa3c9ed61c6bfef9b7665436b0d2
DMRGuide <61344894+DMRGuide@users.noreply.github.com>
2020-02-28 20:47:00 +0300

    DZ1

--- main.c
+++ main.c
@@ -1,4 +1,33 @@
 //1142 Караблин Дмитрий pr1
 #include <stdio.h>
-
-int a = 1;
+#include <stdlib.h>
+#define N 5
+#define M 5
+int main()
+{
+    int A[N][M];
+    {
+        for (int i = 0; i < N; i++) 
+        {
+            for (int j = 0; j < M; j++)
+            {
+                A[i][0] = 1;
+                A[i][4] = 1;
+                A[0][j] = 1;
+                A[4][j] = 1;
+                A[1][1] = rand()%2;
+                A[2][1] = rand()%2;
+                A[3][1] = rand()%2;
+                A[1][2] = rand()%2;
+                A[2][2] = rand()%2;
+                A[3][2] = rand()%2;
+                A[1][3] = rand()%2;
+                A[2][3] = rand()%2;
+                A[3][3] = rand()%2; 
+                printf("%d",A[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    getchar();
+}
